c28a52766583d894278ff8daf5dbadce91ae681a
webztechy <31849814+webztechy@users.noreply.github.com>
2018-06-02 01:42:08 +0400

    Add files via upload

---
+++ assets/js/promo-popup-js.js
@@ -0,0 +1,13 @@
+/*
+ * Project Name: Booking & Reservation
+ * Author by: Renier C. Rumbaoa
+ * Email: renierrumbaoa@gmail.com
+ * Company: Era Information Technology
+ *
+ */
+
+//alert('sdfds');
+jQuery(function (){
+	
+});
+
